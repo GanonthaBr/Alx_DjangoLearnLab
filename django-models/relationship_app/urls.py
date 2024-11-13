bc07fa4009bc2d23491b26ca750dec5157b213a7
Ganontha <bganontha@gmail.com>
2024-11-13 12:47:34 +0100

    impletement user authentication views: login, register, logout

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 from .views import LibrairyDetails
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('books/',views.books,name='book-list'),
-    path('library/<int:pk>',LibrairyDetails.as_view(),name='library-details')
+    path('library/<int:pk>',LibrairyDetails.as_view(),name='library-details'),
+    path('register/',views.register,name='register'),
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
+    
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
 from .models import Book, Library
+from .forms import RegistrationForm
 from django.views.generic.detail import DetailView
 
 # Create your views here.
@@ -14,3 +18,41 @@ class LibrairyDetails(DetailView):
     model = Library
     template_name = 'library_details.html'
     context_object_name = 'library'
+
+
+#registration
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request,user) #log th user in after registration
+            return redirect('book-list')
+    else:
+        form = RegistrationForm()
+        return render(request,'register.html',{'form':form})
+    
+
+#login
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request,data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request,username=username,password=password)
+            if user is not None:
+                login(request,user)
+                return redirect('book-list')
+            else:
+                messages.error(request,'Invalid username or password!')
+    else:
+        form = AuthenticationForm()
+        return render('request','login.html',{'form':form})
+    
+
+#logout
+def logout(request):
+    logout(request)
+    return redirect(login)
