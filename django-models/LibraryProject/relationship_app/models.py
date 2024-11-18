c29e8bdf0b9aefc8dad5c431e3559dbdd38c5cba
Ganontha <bganontha@gmail.com>
2024-11-18 13:25:11 +0100

    Refactoring

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -13,7 +13,6 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-
     class Meta:
         permissions = [
             ("can_add_book","can add book"),
