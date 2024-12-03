5ee06a4b7f771e4c286cb5475cc2011a575c68c1
Ganontha <bganontha@gmail.com>
2024-12-03 21:13:26 +0100

    project basic setup

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -86,13 +86,17 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'your_db_name',
+    #     'USER': 'your_db_user',
+    #     'PASSWORD': 'your_db_password',
+    #     'HOST': 'localhost',  # Set to your database host
+    #     'PORT': '5432',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'your_db_name',
-        'USER': 'your_db_user',
-        'PASSWORD': 'your_db_password',
-        'HOST': 'localhost',  # Set to your database host
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
