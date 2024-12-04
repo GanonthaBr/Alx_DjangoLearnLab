628c5b6a5c198a4d402b2a6bfb822e7bb8cd5e42
Ganontha <bganontha@gmail.com>
2024-12-04 11:17:53 +0100

    Added the register view and the authentication urls

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def home(request):
     posts = Post.objects.all()
     return render(request,'blog/home.html',{'posts':posts})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request,'blog/register.html',{'form':form})
