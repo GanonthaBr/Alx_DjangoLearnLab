46eb515b25541c36ed46cf4255efc95f7a8db4d3
Ganontha <bganontha@gmail.com>
2024-12-12 11:12:32 +0100

    Refactor the routes file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 from .views import *
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.home,name='home'),
+    path('login/',auth_views.LoginView.as_view(template_name='blog/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='blog/login.html'),name='logout'),
+    path('register/',views.register,name='register'),
     path('posts/new/',PostCreateView.as_view(),name='post-create'),
     path('posts/',PostListView.as_view(),name='posts'),
     path('posts/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -1,7 +1,6 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 from blog import views
 #media files
 from django.conf.urls.static import static
@@ -10,9 +9,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
-    path('login/',auth_views.LoginView.as_view(template_name='blog/login.html'),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='blog/login.html'),name='logout'),
-    path('register/',views.register,name='register'),
+    
 ]
 
 urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
