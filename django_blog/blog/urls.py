c3038d9aeeecc67bf5b95e345282c4feb76cac03
Ganontha <bganontha@gmail.com>
2024-12-12 11:28:29 +0100

    update url route for comment

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post-edit'),
     path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post-delete'),
     path('profile/',views.profile_view,name='profile'),
-    path('comment/<int:pk>/edit/',CommentUpdateView.as_view(),name='comment-edit'),
+    path('comment/<int:pk>/update/',CommentUpdateView.as_view(),name='comment-edit'),
     path('comment/<int:pk>/delete/',CommentDeleteView.as_view(),name='comment-delete'),
-    path('comment/<int:pk>/reply/',CommentCreateView.as_view(),name='comment-reply'),
+    path('comment/<int:pk>/comments/new/',CommentCreateView.as_view(),name='comment-reply'),
 
 ]
