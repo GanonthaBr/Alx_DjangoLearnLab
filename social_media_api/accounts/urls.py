77703eb9c7b0a70d8a27ba4663dffefd0bc4f60c
Ganontha <bganontha@gmail.com>
2024-12-16 18:46:48 +0100

    placed /feed route at the posts app

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import RegisterView
 from .views import LoginView
 from .views import ProfileView
-from .views import FollowView, FeedView, LikeView
+from .views import FollowView, LikeView
 from notifications.views import NotificationViewset
 from rest_framework.routers import DefaultRouter
 
@@ -17,7 +17,6 @@ urlpatterns = [
     path('api-token-auth/', LoginView.as_view(), name='api_token_auth'),
     path('follow/<int:user_id>/',FollowView.as_view(),name='follow'),
     path('unfollow/<int:user_id>/',FollowView.as_view(),name='unfollow'),
-    path('feed/',FeedView.as_view(),name='feed'),
     path('posts/<int:pk>/like/',LikeView.as_view(),name='like'),
     path('posts/<int:pk>/unlike/',LikeView.as_view(),name='unlike'),
     path('', include(router.urls)),
