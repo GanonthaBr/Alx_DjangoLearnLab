ba2bf55f50a1d02cdbf9231461e7fe2c36a531d4
Ganontha <bganontha@gmail.com>
2024-12-30 14:02:27 +0100

    Refactor is_admin function to simplify role checking and clean up admin_view formatting

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -97,7 +97,7 @@ class LogoutView(View):
 # Role checking functions
 
 def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+    return user.userprofile.role == 'Admin'
 
 def is_member(user):
     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
@@ -110,9 +110,9 @@ def is_librarian(user):
 
 
 @user_passes_test(is_admin)
-def admin_view(request, ):
+def admin_view(request):
       #your view logic
-       return render(request, 'relationship_app/admin_view.html',
+    return render(request, 'relationship_app/admin_view.html',
     {'message' : "Welcome to Admin Dashboard"})
 # @login_required
 # @user_passes_test(is_admin)
