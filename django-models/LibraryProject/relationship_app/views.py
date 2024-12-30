f10576c40ebda7175d7cc7ba6598633ec1a5974e
Ganontha <bganontha@gmail.com>
2024-12-30 13:58:25 +0100

    Refactor admin view to directly use is_admin for access control

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -107,13 +107,13 @@ def is_librarian(user):
 
 # View controlled access
 
-def admin_test(user):
-    return is_admin(user)
 
-@user_passes_test(admin_test)
-def admin_view(request, *args, **kwargs):
+
+@user_passes_test(is_admin)
+def admin_view(request, ):
       #your view logic
-       return render(*args, **kwargs)
+       return render(request, 'relationship_app/admin_view.html',
+    {'message' : "Welcome to Admin Dashboard"})
 # @login_required
 # @user_passes_test(is_admin)
 # def admin_view(request):
